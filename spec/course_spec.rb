397774f6fe096e4c933e51e04e9eab3a380b6421
Gabriel Torres <thomastgtorres21@gmail.com>
2023-05-24 11:27:04 -0500

    Add attributes

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -5,8 +5,15 @@ require './lib/student'
 RSpec.describe Course do 
   describe '#iteration 2' do
     it 'exist' do
-      Course = course = Course.new("Calculus", 2)  
+      course = Course.new("Calculus", 2)  
       expect(course).to be_an_instance_of(Course)
+    end
+
+    it 'is initialized with attributes' do
+      course = Course.new("Calculus", 2)  
+
+      expect(course.name).to eq("Calculus")
+      expect(course.capacity).to eq(2) 
     end
   end
 end
