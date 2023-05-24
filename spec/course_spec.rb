548d28e548f4da5a08ba42c04c104cd7e103bf04
Gabriel Torres <thomastgtorres21@gmail.com>
2023-05-24 11:17:17 -0500

    Initialize course class

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -3,5 +3,10 @@ require './lib/course'
 require './lib/student'
 
 RSpec.describe Course do 
-
+  describe '#iteration 2' do
+    it 'exist' do
+      Course = course = Course.new("Calculus", 2)  
+      expect(course).to be_an_instance_of(Course)
+    end
+  end
 end
