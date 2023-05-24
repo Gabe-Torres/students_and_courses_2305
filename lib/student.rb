afe2d44c73e7752ac1ca8fff9dec3b86d54c780a
Gabriel Torres <thomastgtorres21@gmail.com>
2023-05-24 10:35:43 -0500

    Add refactors to student

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,11 @@
 class Student
-  
+
     attr_reader :name,
-                :age 
+                :age, 
+                :scores
     def initialize(student_details)
       @name = student_details[:name]
       @age =  student_details[:age]
+      @scores = []
   end
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe Student do
       student = Student.new({name: "Morgan", age: 21})
       expect(student.name).to eq('Morgan')
       expect(student.age).to eq(21)
+      expect(student.scores).to eq([])
     end
   end
 end
