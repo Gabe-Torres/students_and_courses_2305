10d54be9243565302e9511f6b7723fbdb55b3d60
Gabriel Torres <thomastgtorres21@gmail.com>
2023-05-24 10:32:19 -0500

    initial commit

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,9 @@
 class Student
- 
+  
+    attr_reader :name,
+                :age 
+    def initialize(student_details)
+      @name = student_details[:name]
+      @age =  student_details[:age]
+  end
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,5 +2,16 @@ require 'rspec'
 require './lib/student'
 
 RSpec.describe Student do
-  
+  describe '#iteration 1' do
+    it 'exist' do
+      student = Student.new({name: "Morgan", age: 21}) 
+      expect(student).to be_an_instance_of(Student)
+    end
+
+    it 'is initialized with attributes' do
+      student = Student.new({name: "Morgan", age: 21})
+      expect(student.name).to eq('Morgan')
+      expect(student.age).to eq(21)
+    end
+  end
 end
